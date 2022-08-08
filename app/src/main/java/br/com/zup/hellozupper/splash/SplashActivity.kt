225a5a79bf2aaa312150c738b9b5db5254388c2f
tamirescamejo2 <tamires.camejo@zup.com.br>
2022-08-08 16:14:09 -0300

    Implement splash screen

---
+++ app/src/main/java/br/com/zup/hellozupper/splash/SplashActivity.kt
@@ -0,0 +1,28 @@
+package br.com.zup.hellozupper.splash
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.zup.hellozupper.R
+import java.util.*
+
+class SplashActivity : AppCompatActivity() {
+
+    private val timer = Timer()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        timer.schedule(object : TimerTask() {
+            override fun run() {
+                jump()
+            }
+        }, 3000)
+    }
+
+    private fun jump() {
+        timer.cancel()
+        startActivity(Intent(this, LoginActivity::class.java))
+        this.finish()
+    }
+}
