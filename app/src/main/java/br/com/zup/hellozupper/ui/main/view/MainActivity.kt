0fe7e746e6a5f2680ea8b1a02c945ab17472cc9f
luizpaulo451 <luiz.paulo@zup.com.br>
2022-08-09 13:34:56 -0300

    Implement user login

--- app/src/main/java/br/com/zup/hellozupper/ui/main/view/MainActivity.kt
+++ app/src/main/java/br/com/zup/hellozupper/ui/main/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package br.com.zup.hellozupper
+package br.com.zup.hellozupper.ui.main.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.zup.hellozupper.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
