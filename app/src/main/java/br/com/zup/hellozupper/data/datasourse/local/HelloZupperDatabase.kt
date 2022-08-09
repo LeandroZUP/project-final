5350cc47d4178a8108812e653435e97622679981
danielcaitano <daniel.caitano@zup.com.br>
2022-08-09 12:14:26 -0300

    Create Database instance

---
+++ app/src/main/java/br/com/zup/hellozupper/data/datasourse/local/HelloZupperDatabase.kt
@@ -0,0 +1,35 @@
+package br.com.zup.hellozupper.data.datasourse.local
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import br.com.zup.hellozupper.data.datasourse.local.dao.HelloZupperDAO
+
+@Database(entities = [], version = 1)
+abstract class HelloZupperDatabase : RoomDatabase() {
+    abstract fun helloZupperDao(): HelloZupperDAO
+
+    companion object {
+        @Volatile
+        private var INSTANCE: HelloZupperDatabase? = null
+
+        fun getDatabase(context: Context): HelloZupperDatabase {
+            val tempInstance = INSTANCE
+            if (tempInstance != null) {
+                return tempInstance
+            }
+
+            synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    HelloZupperDatabase::class.java,
+                    "hellozupper_database"
+                ).fallbackToDestructiveMigration()
+                    .build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
+}
---
+++ app/src/main/java/br/com/zup/hellozupper/data/datasourse/local/dao/HelloZupperDAO.kt
@@ -0,0 +1,7 @@
+package br.com.zup.hellozupper.data.datasourse.local.dao
+
+import androidx.room.Dao
+
+@Dao
+interface HelloZupperDAO {
+}
