2f24c0c8431d6bf905ac5d49afbd81eea7985eb2
danielcaitano <daniel.caitano@zup.com.br>
2022-08-10 15:42:22 -0300

    Implement Feed Entity

--- app/src/main/java/br/com/zup/hellozupper/data/datasourse/remote/HelloZupperAPI.kt
+++ app/src/main/java/br/com/zup/hellozupper/data/datasourse/remote/HelloZupperAPI.kt
@@ -1,9 +1,13 @@
 package br.com.zup.hellozupper.data.datasourse.remote
 
+import br.com.zup.hellozupper.data.model.FeedResponse
 import br.com.zup.hellozupper.data.model.PillarsResponse
 import retrofit2.http.GET
 
 interface HelloZupperAPI {
     @GET("/pillars")
     suspend fun getPillars(): PillarsResponse
+
+    @GET("/feed")
+    suspend fun getFeed(): FeedResponse
 }
