f11151cd0748b201ea541657530c41bd6c926a6a
LeandroZUP <leandro.almeida@zup.com.br>
2022-08-12 15:34:48 -0300

    Create benefit activities

---
+++ app/src/main/java/br/com/zup/hellozupper/ui/benefit/view/BenefitActivity.kt
@@ -0,0 +1,20 @@
+package br.com.zup.hellozupper.ui.benefit.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import br.com.zup.hellozupper.databinding.ActivityBenefitBinding
+import br.com.zup.hellozupper.ui.benefit.viewmodel.BenefitViewModel
+
+class BenefitActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityBenefitBinding
+    private val viewModel: BenefitViewModel by lazy {
+        ViewModelProvider(this)[BenefitViewModel::class.java]
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityBenefitBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
---
+++ app/src/main/java/br/com/zup/hellozupper/ui/benefit/viewmodel/BenefitViewModel.kt
@@ -0,0 +1,6 @@
+package br.com.zup.hellozupper.ui.benefit.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class BenefitViewModel: ViewModel() {
+}
