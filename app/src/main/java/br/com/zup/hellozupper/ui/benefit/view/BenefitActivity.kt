812c7043755297450db010677756088bad8fe736
luizpaulo451 <luiz.paulo@zup.com.br>
2022-08-12 10:59:02 -0300

    Implement BenefitActivity

---
+++ app/src/main/java/br/com/zup/hellozupper/ui/benefit/view/BenefitActivity.kt
@@ -0,0 +1,11 @@
+package br.com.zup.hellozupper.ui.benefit.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BenefitActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_benefit)
+    }
+}
