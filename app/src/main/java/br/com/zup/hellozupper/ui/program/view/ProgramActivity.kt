a6396e43aeb492dea57cadc48ad25ae564ffe4fd
luizpaulo451 <luiz.paulo@zup.com.br>
2022-08-12 11:05:34 -0300

    Implement ProgramActivity

---
+++ app/src/main/java/br/com/zup/hellozupper/ui/program/view/ProgramActivity.kt
@@ -0,0 +1,11 @@
+package br.com.zup.hellozupper.ui.program.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProgramActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_program)
+    }
+}
