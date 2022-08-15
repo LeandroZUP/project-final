effd4ac28f90c58f0bb9cdf980a6841c187478d6
danielcaitano <110421406+danielcaitano@users.noreply.github.com>
2022-08-15 18:21:09 -0300

    Revert "Feature/benefit"

--- app/src/main/java/br/com/zup/hellozupper/ui/benefit/view/BenefitActivity.kt
+++ app/src/main/java/br/com/zup/hellozupper/ui/benefit/view/BenefitActivity.kt
@@ -1,19 +1,15 @@
 package br.com.zup.hellozupper.ui.benefit.view
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
 import br.com.zup.hellozupper.databinding.ActivityBenefitBinding
-import br.com.zup.hellozupper.ui.benefit.viewmodel.BenefitViewModel
 
 class BenefitActivity : AppCompatActivity() {
     private lateinit var binding: ActivityBenefitBinding
-    private val viewModel: BenefitViewModel by lazy {
-        ViewModelProvider(this)[BenefitViewModel::class.java]
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        binding = ActivityBenefitBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
+        binding = ActivityBenefitBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
