54a1bf45cb813ddcd92ec27f415b286394c74897
luizpaulo451 <luiz.paulo@zup.com.br>
2022-08-15 18:48:47 -0300

    Implement code detailPillarsActivity

---
+++ app/src/main/java/br/com/zup/hellozupper/ui/detailpillars/view/DetailPillarsActivity.kt
@@ -0,0 +1,11 @@
+package br.com.zup.hellozupper.ui.detailpillars.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailPillarsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_pillars)
+    }
+}
