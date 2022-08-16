b549a779b7eb2005d78de4ee55e0150cf7e6fec6
LeandroZUP <leandro.almeida@zup.com.br>
2022-08-16 10:56:01 -0300

    Create benefit response model

--- app/src/main/java/br/com/zup/hellozupper/data/model/BenefitResponse.kt
+++ app/src/main/java/br/com/zup/hellozupper/data/model/BenefitResponse.kt
@@ -1,3 +1,8 @@
 package br.com.zup.hellozupper.data.model
 
-data class BenefitResponse()
+import com.google.gson.annotations.SerializedName
+
+data class BenefitResponse(
+@SerializedName("benefits")
+val benefits: List<Benefit>
+)
