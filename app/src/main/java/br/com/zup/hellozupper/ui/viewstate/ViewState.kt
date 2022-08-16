562a3193294088411e1fb6fb689b95c67bd815e4
danielcaitano <110421406+danielcaitano@users.noreply.github.com>
2022-08-16 19:39:04 -0300

    Revert "Feature/benefit"

--- app/src/main/java/br/com/zup/hellozupper/ui/viewstate/ViewState.kt
+++
@@ -1,7 +0,0 @@
-package br.com.zup.hellozupper.ui.viewstate
-
-sealed class ViewState<out T> {
-    data class Success<T>(val data: T) : ViewState<T>()
-    data class Error(val throwable: Throwable) : ViewState<Nothing>()
-    data class Loading(val loading: Boolean) : ViewState<Nothing>()
-}
