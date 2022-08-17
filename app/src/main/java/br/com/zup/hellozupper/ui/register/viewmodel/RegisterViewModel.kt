ce36d40c259b15843ded9c6f550b8c84cd411507
luizpaulo451 <luiz.paulo@zup.com.br>
2022-08-17 16:52:00 -0300

    Update icon AndroidManifest

--- app/src/main/java/br/com/zup/hellozupper/ui/register/viewmodel/RegisterViewModel.kt
+++ app/src/main/java/br/com/zup/hellozupper/ui/register/viewmodel/RegisterViewModel.kt
@@ -80,7 +80,6 @@ class RegisterViewModel : ViewModel() {
                 user.email,
                 user.password
             ).addOnSuccessListener {
-
                 authenticationRepository.updateUserProfile(user.name)?.addOnSuccessListener {
                     _registerState.value = user
                 }
